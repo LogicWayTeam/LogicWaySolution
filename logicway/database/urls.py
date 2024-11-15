289a172472b6022f503617deb5741b7c205ab092
Tsybulka Mikita <nikita.tsybulka@gmail.com>
2024-11-15 14:12:18 +0100

    [fixed] bug with api/

--- logicway/database/urls.py
+++ logicway/database/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/stops/', views.get_stops, name='get_stops'),
-    path('api/stop/<str:stop_name>/', views.get_stop, name='get_stop'),
-    path('api/route/<str:route_id>/<int:direction>/', views.get_route, name='get_route'),
+    path('stops/', views.get_stops, name='get_stops'),
+    path('stop/<str:stop_name>/', views.get_stop, name='get_stop'),
+    path('route/<str:route_id>/<int:direction>/', views.get_route, name='get_route'),
 ]
