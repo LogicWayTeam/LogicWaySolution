545d0219c10a8c965ea2b32e4a64e461ea122cf5
Hihihikka <hihikka22@gmail.com>
2025-05-06 10:22:49 +0300

    Rename UseStops.js to useStops.js

---
+++ logicway/frontend/src/components/useStops.js
@@ -0,0 +1,45 @@
+import { useEffect, useState, useRef } from 'react';
+import L from 'leaflet';
+
+const useStops = (map) => {
+  const [stops, setStops] = useState([]);
+  const markersLayerRef = useRef(L.layerGroup());
+
+  useEffect(() => {
+    // Загружаем остановки с сервера
+    fetch('http://127.0.0.1:8000/api/stops/')
+      .then(res => res.json())
+      .then(data => {
+        setStops(data);
+      })
+      .catch(err => console.error('Error fetching stops:', err));
+  }, []);
+
+  useEffect(() => {
+    // Добавляем остановки на карту
+    const layerGroup = markersLayerRef.current;
+    layerGroup.clearLayers();
+
+    stops.forEach(stop => {
+      const { stop_lat, stop_lon, stop_name } = stop;
+
+      L.circle([stop_lat, stop_lon], {
+        color: '#931050',
+        fillColor: '#931050',
+        fillOpacity: 0.5,
+        radius: 2,
+      }).bindPopup(`<b>${stop_name}</b><br>Lat: ${stop_lat}, Lon: ${stop_lon}`)
+        .addTo(layerGroup);
+    });
+
+    layerGroup.addTo(map);
+
+    return () => {
+      map.removeLayer(layerGroup);
+    };
+  }, [stops, map]);
+
+  return stops;
+};
+
+export default useStops;
