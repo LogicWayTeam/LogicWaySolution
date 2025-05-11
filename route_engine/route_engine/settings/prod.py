872b97847db319e07af457e62994c5001e5fb829
Mikita Tsybulka <nikita.tsybulka@gmail.com>
2025-05-11 12:15:38 +0200

    [fixed] by setting correct ALLOWED_HOSTS + [updated] libs

--- route_engine/route_engine/settings/prod.py
+++ route_engine/route_engine/settings/prod.py
@@ -2,11 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.taile241c6.ts.net'
-]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+ALLOWED_HOSTS.append('.taile241c6.ts.net')
