8ec2a053345c2e101ee77faa2718ea589c353c15
Mikita Tsybulka <nikita.tsybulka@gmail.com>
2025-05-24 17:37:09 +0200

    [added] to CORS_ALLOWED_ORIGINS pre-prod urls

--- logicway/logicway/settings/base.py
+++ logicway/logicway/settings/base.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://logicway.taile241c6.ts.net:3000",
+    "http://logicway-k8s.taile241c6.ts.net:3000",
 ]
 
 # Application definition
