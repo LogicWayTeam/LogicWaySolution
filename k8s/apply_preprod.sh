45f2b338cf92a9bb0568730b0fc7ec407edf6288
Mikita Tsybulka <nikita.tsybulka@gmail.com>
2025-08-21 14:01:33 +0200

    [created] k8s apply scripts

---
+++ k8s/apply_preprod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# --- apply configmaps and secrets ---
+kubectl apply -f preprod-env/configmap.yaml
+kubectl apply -f base/secret.yaml
+
+# --- apply deployments ---
+kubectl apply -f base/frontend/frontend-deployment.yaml
+kubectl apply -f base/logicway/logicway-deployment.yaml
+kubectl apply -f base/postgres/postgres-deployment.yaml
+kubectl apply -f base/route-engine/route-engine-deployment.yaml
+
+# --- apply services ---
+kubectl apply -f preprod-env/services/
+
+# --- apply jobs ---
+kubectl apply -f base/logicway/job-load-data.yaml
