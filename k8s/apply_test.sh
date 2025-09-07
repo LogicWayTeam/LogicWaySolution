76863fbd36b2700a8b328b9812694f333ec75ed2
Mikita Tsybulka <nikita.tsybulka@gmail.com>
2025-09-07 20:05:34 +0200

    [fixed] path in apply_test script

--- k8s/apply_test.sh
+++ k8s/apply_test.sh
@@ -2,16 +2,16 @@
 
 # --- apply configmaps and secrets ---
 kubectl apply -f test-env/configmap.yaml -n test
-kubectl apply -f test-env/secret.yaml -n test
+kubectl apply -f base/secret.yaml -n test
 
 # --- apply deployments ---
-kubectl apply -f test-env/frontend/frontend-deployment.yaml -n test
-kubectl apply -f test-env/logicway/logicway-deployment.yaml -n test
-kubectl apply -f test-env/postgres/postgres-deployment.yaml -n test
-kubectl apply -f test-env/route-engine/route-engine-deployment.yaml -n test
+kubectl apply -f base/frontend/frontend-deployment.yaml -n test
+kubectl apply -f base/logicway/logicway-deployment.yaml -n test
+kubectl apply -f base/postgres/postgres-deployment.yaml -n test
+kubectl apply -f base/route-engine/route-engine-deployment.yaml -n test
 
 # --- apply services ---
 kubectl apply -f test-env/services/ -n test
 
 # --- apply jobs ---
-kubectl apply -f test-env/logicway/job-load-data.yaml -n test
+kubectl apply -f base/logicway/job-load-data.yaml -n test
